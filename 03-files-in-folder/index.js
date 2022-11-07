b01b177190bcbd09326cf170433258047c47ad8c
osipik.a <osipik.av@yandex.by>
2022-11-07 21:14:19 +0300

    refactor: solution of task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,10 +5,8 @@ const folderPath = path.join(__dirname, "secret-folder");
 
 fs.readdir(folderPath, { withFileTypes: true }, (err, data) => {
   data.forEach((item) => {
-    // console.log("item :>> ", item.stat().size);
     if (item.isFile()) {
       fs.stat(path.join(folderPath, item.name), (err, x) => {
-        // console.log(path.basename(item.name));
         console.log(`${item.name.split(".")[0]} - ${path.extname(item.name).slice(1)} - ${x.size / 1000}kb`);
       });
     }
