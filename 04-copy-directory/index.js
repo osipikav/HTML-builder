3b68745a0fb6da3d9c437fff4f9d69ff5771be91
osipik.a <osipik.av@yandex.by>
2022-11-08 18:35:03 +0300

    feat: solution of task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require("fs");
+const path = require("path");
 
+const folderPath = path.join(__dirname, "files");
+const newFolderPath = path.join(__dirname, "files-copy");
+
+fs.rm(newFolderPath, { recursive: true, force: true }, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  fs.readdir(path.join(folderPath), { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    fs.mkdir(newFolderPath, { recursive: true, force: true }, (err) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        fs.copyFile(path.join(folderPath, file.name), path.join(newFolderPath, file.name), (err) => {
+          if (err) throw err;
+        });
+      });
+    });
+  });
+});
